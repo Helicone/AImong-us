912a66aae50cbec9be72730e638691488ddba784
Calum Bird <calum@trelent.net>
2023-04-01 21:20:14 -0300

    Almost pretty

--- components/games/oddBotOut/activeGame.tsx
+++ components/games/oddBotOut/activeGame.tsx
@@ -16,15 +16,37 @@ export default function ActiveGame(props: ActiveGameProps) {
     TOTAL_TIME_TO_ANSWER_QUESTION -
     (Date.now() - new Date(currentQuestion.created_at!).getTime());
 
+  const stateColor = (game_state: string) => {
+    switch (game_state) {
+      case "finding_players":
+        return "bg-yellow-300";
+      case "active":
+        return "bg-green-300";
+      case "finished":
+        return "bg-red-300";
+      default:
+        return "bg-red-300";
+    }
+  };
+
   return (
-    <div>
-      <div>
-        <div>{game?.game_state}</div>
-        {timeLeft}
-        <div>
-          {game?.player_count} / {NUM_PLAYERS} Players Joined
-          {game?.questions[0].question}
+    <div className="flex flex-col max-w-2xl content-center">
+      <div className="flex flex-row justify-between col-span-2">
+        <div className="flex flex-col">
+          <div
+            className={
+              "align-start p-2 text-white font-bold rounded-lg " +
+              stateColor(game.game_state)
+            }
+          >
+            {game.game_state}
+          </div>
+          {game.player_count} / {NUM_PLAYERS} Players Joined
         </div>
+        <div className="flex flex-col">{timeLeft}</div>
+      </div>
+      <div className="flex flex-col">
+        <div>{game.questions[0].question}</div>
       </div>
     </div>
   );
