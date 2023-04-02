250cca915fa9940f05535f6ee11a1e5242b75585
ingawei <ingawei@gmail.com>
2023-04-01 18:32:55 -0700

    editing finding players page

--- components/games/oddBotOut/findingPlayers.tsx
+++ components/games/oddBotOut/findingPlayers.tsx
@@ -1,19 +1,66 @@
+import clsx from "clsx";
 import { NUM_PLAYERS } from "../../../lib/constants";
 import { GameResponse } from "../../../pages/api/odd-bot-out/game";
-
+import { Col } from "../../layout/col";
+import { Row } from "../../layout/row";
+import { Spacer } from "../../layout/spacer";
+import Lottie from "react-lottie";
+import searching from "../../../public/lottie/finding.json";
 interface FindingPlayersProps {
-  game: GameResponse;
+  game: NonNullable<GameResponse>;
 }
+
 export default function FindingPlayers(props: FindingPlayersProps) {
   const { game } = props;
   return (
-    <div>
-      <div>
-        <div>{game?.game_state}</div>
-        <div>
-          {game?.player_count} / {NUM_PLAYERS} Players Joined
+    <Col className="h-full">
+      <Col className="items-center gap-2">
+        <Row className="justify-center">
+          <Lottie
+            options={{
+              loop: true,
+              autoplay: true,
+              animationData: searching,
+              rendererSettings: {
+                preserveAspectRatio: "xMidYMid slice",
+              },
+            }}
+            height={200}
+            width={200}
+            isStopped={false}
+            isPaused={false}
+            style={{
+              color: "#6366f1",
+              pointerEvents: "none",
+              background: "transparent",
+            }}
+          />
+        </Row>
+        <div className="text-xl">Finding players...</div>
+        <div className="text-sm right-0 bottom-0">
+          {game?.player_count} / {NUM_PLAYERS}
         </div>
-      </div>
-    </div>
+      </Col>
+      <Spacer h={6} />
+      <Col className="gap-4 mx-4">
+        {Array.from({ length: NUM_PLAYERS }).map((_, i) => (
+          <MysteryAvatar key={i} isFound={i < game?.player_count} />
+        ))}
+      </Col>
+    </Col>
+  );
+}
+
+export function MysteryAvatar(props: { isFound: boolean }) {
+  const { isFound } = props;
+  const avatarClass = clsx(
+    "transition-colors",
+    isFound ? "bg-gray-600" : "bg-gray-300 animate-pulse"
+  );
+  return (
+    <Row className="gap-3 items-center">
+      <div className={clsx("w-8 h-8 rounded-full", avatarClass)} />
+      <div className={clsx(" grow h-4 ", avatarClass)} />
+    </Row>
   );
 }
--- components/mainWrapper.tsx
+++ components/mainWrapper.tsx
@@ -1,3 +1,5 @@
+import { Col } from "./layout/col";
+
 interface MainWrapperProps {
   children: React.ReactNode;
   title: string;
@@ -6,16 +8,16 @@ interface MainWrapperProps {
 export function MainWrapper(props: MainWrapperProps) {
   const { children, title } = props;
   return (
-    <main className="flex flex-col w-full flex-1 text-center min-h-screen justify-between">
-      <h1 className="text-center text-4xl font-bold w-full bg-white py-5 border-b px-5">
+    <main className="flex flex-col w-full flex-1 min-h-screen justify-between bg-violet-50">
+      <h1 className="text-center w-full py-2 border-b px-5 font-mono">
         <div className="">
-          <div className="flex  md:flex-row w-full justify-between items-center">
+          <div className="flex md:flex-row w-full justify-between items-center">
             <div className="text-center w-full">{title}</div>
           </div>
         </div>
       </h1>
-      {children}
-      <div className="w-full pb-5 pt-2 bg-white flex flex-col gap-3 justify-center items-center">
+      <Col className="grow px-6 py-4">{children}</Col>
+      <div className="w-full pb-5 pt-2 flex flex-col gap-3 justify-center items-center">
         <div className="flex flex-row gap-5">
           <a href="https://twitter.com/valyr_ai">
             <i>Twitter</i>
