8c18053a38cd80985640fc62d0cb9ce528046850
Anton Lazarev <antonok35@gmail.com>
2023-04-26 22:12:17 -0700

    remove junk

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Analytics } from "@vercel/analytics/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { useState } from "react";
-import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
 const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
-  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
   if (typeof window !== "undefined") {
     document.documentElement.classList.add("dark");
   }
 
   return (
     <>
-      <SessionContextProvider
-        supabaseClient={supabaseClient}
-        initialSession={pageProps.initialSession}
-      >
-        <QueryClientProvider client={queryClient}>
-          <Component {...pageProps} />
-        </QueryClientProvider>
-      </SessionContextProvider>
-      <Analytics />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
-import { useUser } from "@supabase/auth-helpers-react";
 import Head from "next/head";
 import { MainWrapper } from "../components/mainWrapper";
 
 import Lobby from "../components/games/oddBotOut/lobby";
 
 export default function Home() {
-  const user = useUser();
   return (
     <div className="bg-cyan-100">
       <Head>
