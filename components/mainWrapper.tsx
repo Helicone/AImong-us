4ace550fb5f2e9926ac1a10ac7f15ee87be45317
Justin <justintorre75@gmail.com>
2023-05-14 19:36:10 -0700

    fixed header

--- components/mainWrapper.tsx
+++ components/mainWrapper.tsx
@@ -3,6 +3,8 @@ import { ClientGameStateView } from "../aimongus_types/bindings/ClientGameStateV
 import StarBackground from "./games/aimongus/star";
 import { Col } from "./layout/col";
 
+import { BsDiscord, BsGithub } from "react-icons/bs";
+
 interface MainWrapperProps {
   children: React.ReactNode;
   title: string;
@@ -10,17 +12,38 @@ interface MainWrapperProps {
 }
 
 import React from "react";
+import { Row } from "./layout/row";
 
 export function MainWrapper(props: MainWrapperProps) {
   const { children, title, game } = props;
   return (
     <main className="w-screen h-screen bg-black">
       <div className="z-10 text-white">
-        <h1 className="text-center w-full py-2 px-5 z-10 bg-violet-950">
-          <div className="text-center w-full text-md font-mono">
-            AImong.us (alpha)
-          </div>
-        </h1>
+        <div className="w-full bg-violet-950">
+          <Row className="w-full justify-between  z-10 max-w-2xl">
+            <div></div>
+            <h1
+              className="text-center w-full py-2 px-5 z-10 hover:opacity-80 cursor-pointer"
+              onClick={() => window.open("/", "_self")}
+            >
+              <div className="text-center w-full text-md font-mono">
+                AImong.us (alpha)
+              </div>
+            </h1>
+            <Row className="gap-2">
+              <BsDiscord
+                className=" text-white z-10  h-10 hover:opacity-80 cursor-pointer"
+                onClick={() =>
+                  window.open("https://discord.gg/hySsN9KCSt", "_blank")
+                }
+              />
+              {/* <BsGithub
+                className=" text-white z-10  h-10 hover:opacity-80 cursor-pointer"
+                onClick={() => window.open("", "_blank")}
+              /> */}
+            </Row>
+          </Row>
+        </div>
         <Col className="grow p-2 sm:px-6 sm:py-4 z-10">{children}</Col>
       </div>
     </main>
